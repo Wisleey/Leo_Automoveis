ace3b9855f54443da9ef72c7082228ba9b82b50a
Wisleey <wisley_killat@hotmail.com>
2024-04-23 12:27:32 -0300

    alteração div azul

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -17,34 +17,34 @@ export function Header() {
           <Link to="/">
             <img src={logoImg} alt="Logo do site" />
           </Link>
-          
-          <nav>
-            <ul className="flex items-center space-x-4">
-              <li>
-                <Link to="/" className="text-black">Início</Link>
-              </li>
-              <li>
-                <Link to="/car/car" className="text-black">Carros</Link>
-              </li>
-              <li>
-                <Link to="/login" className="text-black">Login</Link>
-              </li>
-              <li>
-                <Link to="/" className="text-black" onClick={handleQueroVenderClick}>Quero vender!!</Link>
-              </li>
-              <li>
-                <a
-                  href="https://api.whatsapp.com/send?phone=5583991812589"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center bg-green-500 text-white px-3 py-2 rounded-md"
-                >
-                  WhatsApp
-                  <FontAwesomeIcon icon={faWhatsapp} className="ml-2" />
-                </a>
-              </li>
-            </ul>
-          </nav>
+          <nav className="flex justify-center">
+  <ul className="flex items-center space-x-4">
+    <li>
+      <Link to="/" className="text-black">Início</Link>
+    </li>
+    <li>
+      <Link to="/car/car" className="text-black">Carros</Link>
+    </li>
+    <li>
+      <Link to="/login" className="text-black">Login</Link>
+    </li>
+    <li>
+      <Link to="/" className="text-black" onClick={handleQueroVenderClick}>Quero vender!!</Link>
+    </li>
+    <li>
+      <a
+        href="https://api.whatsapp.com/send?phone=5583991812589"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center bg-green-500 text-white px-3 py-2 rounded-md"
+      >
+        WhatsApp
+        <FontAwesomeIcon icon={faWhatsapp} className="ml-2" />
+      </a>
+    </li>
+  </ul>
+</nav>
+
         </header>
       </div>
     </div>
